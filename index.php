c24e086bfb19ab2cdd6e1d1c559a6bfb220a33b9
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:15:22 +0100

    exercice7

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$isOk = false;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>php partie2</title>
+</head>
+<body>
+    <?php
+    if(!$isOk){ ?>
+      <p>C'est pas bon !!!</p>
+  <?php }else{ ?>
+      <p>C'est ok !!</p>
+  <?php } ?>
+</body>
+</html>
